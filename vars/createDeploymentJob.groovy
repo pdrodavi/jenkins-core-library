35b56ee27689715d5d0ce10624c17f86073fafe0
Pedro Davi <pedrodaviti@gmail.com>
2023-02-24 05:19:20 -0300

    dsl job

--- vars/createDeploymentJob.groovy
+++ vars/createDeploymentJob.groovy
@@ -1,6 +1,5 @@
 import br.dev.pedrodavi.jenkins.pipeline.job.PipelineJob
 import javaposse.jobdsl.dsl.DslFactory
-import javaposse.jobdsl.dsl.jobs.FreeStyleJob
 import br.dev.pedrodavi.jenkins.pipeline.Constants
 
 def call() {
