4c946da71c482d3de2246dd0adbc31878b74ffd7
Pedro Davi <pedrodaviti@gmail.com>
2023-02-24 05:26:31 -0300

    dsl job

--- vars/createJob.groovy
+++ vars/createJob.groovy
@@ -2,18 +2,22 @@ import javaposse.jobdsl.dsl.DslFactory
 
 def call(DslFactory dslFactory, String pipelineId, String pipelineName, String gitRemoteURL, String credentialsId) {
 
-    dslFactory.freeStyleJob(pipelineId) {
-        description("test")
-        displayName(pipelineName)
-        keepDependencies(false)
-        scm {
-            git {
-                remote {
-                    name('origin')
-                    url(gitRemoteURL)
-                    credentials(credentialsId)
+    Boolean executeStage = true
+
+    conditionalStage("createJob", executeStage) {
+        dslFactory.freeStyleJob(pipelineId) {
+            description("test")
+            displayName(pipelineName)
+            keepDependencies(false)
+            scm {
+                git {
+                    remote {
+                        name('origin')
+                        url(gitRemoteURL)
+                        credentials(credentialsId)
+                    }
+                    branch('main')
                 }
-                branch('main')
             }
         }
     }
