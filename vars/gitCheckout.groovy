9b53440224a6d1cadd119e41f7505412f2b99534
Pedro Davi <pedrodaviti@gmail.com>
2023-02-23 22:34:37 -0300

    include stages conditionals

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,10 +1,6 @@
 import br.dev.pedrodavi.jenkins.pipeline.Constants
-import br.dev.pedrodavi.jenkins.pipeline.utils.ConfigConstants
-//import br.dev.pedrodavi.jenkins.pipeline.utils.logging.Logger
 
 def call(String repo){
-
-//    Logger log = new Logger("checkoutStage")
 
     inputBranch = input([
             message: 'Input branch',
@@ -13,16 +9,7 @@ def call(String repo){
             ]
     ])
 
-    checkoutScm(
-            (SCM) : [
-                    (SCM_URL) : "https://github.com/pdrodavi/${repo}.git",
-                    (SCM_CREDENTIALS_ID) : Constants.JENKINS_GITHUB_CREDENTIALS_ID,
-                    (SCM_BRANCHES) : [[name: '*/main'], [name: '*/develop']],
-            ]
-    )
-
     echo "Branch selecionada: ${inputBranch}"
-//    log.info("Branch selecionada: ${inputBranch}")
 
     git branch: "${inputBranch}", credentialsId: Constants.JENKINS_GITHUB_CREDENTIALS_ID, url: "https://github.com/pdrodavi/${repo}.git"
 
