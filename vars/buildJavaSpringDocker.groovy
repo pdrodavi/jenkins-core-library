70110cbf95ab9974181b7b3b97cd95c203f9673a
Pedro Davi <pedrodaviti@gmail.com>
2023-02-23 22:42:42 -0300

    test setupTools

--- vars/buildJavaSpringDocker.groovy
+++ vars/buildJavaSpringDocker.groovy
@@ -1,5 +1,21 @@
+import br.dev.pedrodavi.jenkins.pipeline.model.Tool
+import br.dev.pedrodavi.jenkins.pipeline.utils.logging.*
+import static br.dev.pedrodavi.jenkins.pipeline.utils.ConfigConstants.*
+
 def call(Map args) {
+
+    Map config = [
+            (TOOLS): [
+                    [ (TOOL_NAME): 'M3', (TOOL_TYPE): Tool.MAVEN ]
+            ],
+            (LOGLEVEL): LogLevel.INFO
+    ]
+
+    Logger.init(this, config)
+
     node {
+
+        setupTools(config)
 
         stage("Checkout") {
             gitCheckout(args.repo)
