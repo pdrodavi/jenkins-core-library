4c7c69dcbddbde8a119bf026ed3ff958eb0ad89c
Pedro Davi <pedrodaviti@gmail.com>
2023-02-24 06:06:20 -0300

    dsl job

--- vars/createDeploymentJob.groovy
+++ vars/createDeploymentJob.groovy
@@ -2,6 +2,8 @@ import br.dev.pedrodavi.jenkins.pipeline.builder.GradleCiJobBuilder
 import br.dev.pedrodavi.jenkins.pipeline.job.PipelineJob
 import javaposse.jobdsl.dsl.DslFactory
 import br.dev.pedrodavi.jenkins.pipeline.Constants
+import javaposse.jobdsl.dsl.Job
+import javaposse.jobdsl.dsl.JobParent
 
 def call() {
 
@@ -17,8 +19,15 @@ def call() {
             GradleCiJobBuilder builder = new GradleCiJobBuilder()
                     .name('test-job')
                     .description('testing')
-                    .ownerAndProject('sheehan/example')
-                    .tasks('clean test')
+                    .ownerAndProject('pdrodavi/app-quarkus-job-deploy')
+                    .tasks('clean')
+            JobParent jobParent = new JobParent() {
+                @Override
+                Object run() {
+                    Job job = builder.build(jobParent)
+                    return job
+                }
+            }
         }
 
 //        createJob(this as DslFactory, "EXAMPLE_JOB_ID", "EXAMPLE_JOB_NAME", "https://github.com/pdrodavi/app-quarkus-job-deploy.git", Constants.JENKINS_GITHUB_CREDENTIALS_ID)
