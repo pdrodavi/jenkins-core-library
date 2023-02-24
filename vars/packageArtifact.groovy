5761c264994fddfb9b54fdb4b2d5d182faba36e2
Pedro Davi <pedrodaviti@gmail.com>
2023-02-23 23:48:30 -0300

    log

--- vars/packageArtifact.groovy
+++ vars/packageArtifact.groovy
@@ -1,19 +1,6 @@
-import br.dev.pedrodavi.jenkins.pipeline.Demo
-import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
-import br.dev.pedrodavi.jenkins.pipeline.utils.logging.*
-
 def call() {
-
-    Demo demo = new Demo()
-    demo.sayHello()
-
-    Logger.init(this, LogLevel.INFO)
-    Logger log = new Logger(this)
-
-    log.info(readMavenPom().getArtifactId())
-    log.info("Realizando construção do artefato")
-//    echo readMavenPom().getArtifactId()
-//    echo readMavenPom().getVersion()
-//    echo "Realizando construção do artefato"
+    echo readMavenPom().getArtifactId()
+    echo readMavenPom().getVersion()
+    echo "Realizando construção do artefato"
     sh "mvn -Dmaven.test.skip=true -Dmaven.test.failure.ignore clean package"
 }
