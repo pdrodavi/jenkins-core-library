8e6d6832fe0011d5fdad863e4128f9b065dabaa4
Pedro Davi <pedrodaviti@gmail.com>
2023-02-24 02:25:02 -0300

    log

---
+++ vars/sampleJob.groovy
@@ -0,0 +1,15 @@
+def call() {
+
+    job ('SampleJenkinsJob') {
+
+        logRotator {
+            daysToKeep(10)
+            numToKeep(-1)
+        }
+        steps {
+            shell("echo 'hello world'")
+        }
+    }
+
+
+}
