10cdc170cbd0cfe829e86ced8993a4400f9c622a
Pedro Davi <pedrodaviti@gmail.com>
2023-03-01 23:33:13 -0300

    update

--- vars/getBranchesRepoGithub.groovy
+++ vars/getBranchesRepoGithub.groovy
@@ -6,6 +6,7 @@ def call() {
 
         //httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: false, name: 'Accept', value: 'application/vnd.github+json'], [maskValue: false, name: 'Authorization', value: "Bearer ${GITHUBRESTJWT}"], [maskValue: false, name: 'X-GitHub-Api-Version', value: '2022-11-28']], responseHandle: 'NONE', url: 'https://api.github.com/repos/Srvex/example-auth-jwt/branches', wrapAsMultipart: false
         httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: false, name: 'Accept', value: 'application/vnd.github+json'], [maskValue: false, name: 'Authorization', value: "Bearer ${GITHUBRESTJWT}"], [maskValue: false, name: 'X-GitHub-Api-Version', value: '2022-11-28']], outputFile: 'branches.json', url: 'https://api.github.com/repos/Srvex/example-auth-jwt/branches', wrapAsMultipart: false
+        readJSON file: 'branches.json', text: ''
 
     }
 
