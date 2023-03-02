1f1ce264c99696396a50789eb2701c8d69c43834
Pedro Davi <pedrodaviti@gmail.com>
2023-03-01 22:24:41 -0300

    test

--- vars/restGetURL.groovy
+++ vars/restGetURL.groovy
@@ -2,29 +2,31 @@
 import com.cloudbees.groovy.cps.NonCPS
 import groovy.json.JsonSlurperClassic
 
-def call(body) {
+def call(url) {
     // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+    //def config = [:]
+    //body.resolveStrategy = Closure.DELEGATE_FIRST
+    //body.delegate = config
+    //body()
 
-    if (config.url == null) {
+    if (url == null) {
         error "No URL found"
     }
 
     retry(3) {
-        return getResult(config.url, config.authString)
+        return getResult(url)
     }
 }
 
 @NonCPS
-def getResult(url, authString) {
+//def getResult(url, authString) {
+def getResult(url) {
     echo "${url}"
     HttpURLConnection connection = url.openConnection()
+    /*
     if (authString != null && authString.length() > 0) {
         connection.setRequestProperty("Authorization", "Bearer ${authString}")
-    }
+    }*/
     connection.setRequestMethod("GET")
     connection.setDoInput(true)
     def rs = null
