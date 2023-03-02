87ce67b8bf3a4d4a9c823734e7ccc78994093c32
Pedro Davi <pedrodaviti@gmail.com>
2023-03-01 23:26:02 -0300

    update

--- vars/getBranchesRepoGithub.groovy
+++ vars/getBranchesRepoGithub.groovy
@@ -4,7 +4,8 @@ def call() {
 
     withCredentials([string(credentialsId: Constants.JENKINS_GITHUB_REST_CREDENTIALS_ID, variable: 'GITHUBRESTJWT')]) {
 
-        httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: false, name: 'Accept', value: 'application/vnd.github+json'], [maskValue: false, name: 'Authorization', value: "Bearer ${GITHUBRESTJWT}"], [maskValue: false, name: 'X-GitHub-Api-Version', value: '2022-11-28']], responseHandle: 'NONE', url: 'https://api.github.com/repos/Srvex/example-auth-jwt/branches', wrapAsMultipart: false
+        //httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: false, name: 'Accept', value: 'application/vnd.github+json'], [maskValue: false, name: 'Authorization', value: "Bearer ${GITHUBRESTJWT}"], [maskValue: false, name: 'X-GitHub-Api-Version', value: '2022-11-28']], responseHandle: 'NONE', url: 'https://api.github.com/repos/Srvex/example-auth-jwt/branches', wrapAsMultipart: false
+        httpRequest consoleLogResponseBody: true, customHeaders: [[maskValue: false, name: 'Accept', value: 'application/vnd.github+json'], [maskValue: false, name: 'Authorization', value: "Bearer ${GITHUBRESTJWT}"], [maskValue: false, name: 'X-GitHub-Api-Version', value: '2022-11-28']], outputFile: 'branches.json', url: 'https://api.github.com/repos/Srvex/example-auth-jwt/branches', wrapAsMultipart: false
 
     }
 
