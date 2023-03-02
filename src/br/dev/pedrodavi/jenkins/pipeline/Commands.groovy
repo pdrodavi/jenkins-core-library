7f0d48549ef6085a1fd7f61ca4712e7cb63d6ec3
Pedro Davi <pedrodaviti@gmail.com>
2023-03-01 23:00:36 -0300

    getProjectVersion

--- src/br/dev/pedrodavi/jenkins/pipeline/Commands.groovy
+++ src/br/dev/pedrodavi/jenkins/pipeline/Commands.groovy
@@ -1,2 +1,14 @@
 package br.dev.pedrodavi.jenkins.pipeline
 
+import com.cloudbees.groovy.cps.NonCPS
+
+class Commands implements Serializable {
+
+    @NonCPS
+    static String getProjectVersion() {
+        def file = readFile('pom.xml')
+        def project = new XmlSlurper().parseText(file)
+        return project.version.text()
+    }
+
+}
