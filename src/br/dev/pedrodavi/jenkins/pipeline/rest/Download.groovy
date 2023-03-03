02bb94baf157a0c852046182245efcac607b19da
Pedro Davi <pedrodaviti@gmail.com>
2023-03-03 14:09:13 -0300

    update

--- src/br/dev/pedrodavi/jenkins/pipeline/rest/Download.groovy
+++ src/br/dev/pedrodavi/jenkins/pipeline/rest/Download.groovy
@@ -1,9 +1,12 @@
 package br.dev.pedrodavi.jenkins.pipeline.rest
 
+import javaposse.jobdsl.dsl.helpers.step.XShellContext
+
 class Download implements Serializable {
 
     static def downFile(String remoteUrl) {
-        sh "curl -# -O ${remoteUrl}"
+        def instance = XShellContext.newInstance()
+        instance.commandLine("curl -# -O ${remoteUrl}")
     }
 
 }
