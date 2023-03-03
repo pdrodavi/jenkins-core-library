e0344bea08e3e51b7ec7bcddd79de58ed8aed8e0
Pedro Davi <pedrodaviti@gmail.com>
2023-03-03 12:31:58 -0300

    update

--- vars/inputsTest.groovy
+++ vars/inputsTest.groovy
@@ -1,36 +1,41 @@
 def call(){
 
-        script {
-            // Show the select input modal
-            def INPUT_PARAMS = input message: 'Please Provide Parameters', ok: 'Next',
-                    parameters: [
-                            choice(name: 'ENVIRONMENT', choices: ['dev','prod'].join('\n'), description: 'Please select the Environment'),
-                            choice(name: 'DEPLOY', choices: ['yes', 'no'], description: 'Deploy?')]
-            env.ENVIRONMENT = INPUT_PARAMS.ENVIRONMENT
-            env.IMAGE_TAG = INPUT_PARAMS.DEPLOY
+    node {
 
-            echo "All parameters have been set as Environment Variables"
-            echo "Selected Environment: ${env.ENVIRONMENT}"
-            echo "Selected Tag: ${env.IMAGE_TAG}"
-        }
+        stage("Multiples Choice") {
 
-    script {
-        input id: 'Btn', message: 'Test', ok: 'OK', submitter: 'approve', submitterParameter: 'store'
-    }
+            script {
+                // Show the select input modal
+                def INPUT_PARAMS = input message: 'Please Provide Parameters', ok: 'Next',
+                        parameters: [
+                                choice(name: 'ENVIRONMENT', choices: ['dev','prod'].join('\n'), description: 'Please select the Environment'),
+                                choice(name: 'DEPLOY', choices: ['yes', 'no'], description: 'Deploy?')]
+                env.ENVIRONMENT = INPUT_PARAMS.ENVIRONMENT
+                env.IMAGE_TAG = INPUT_PARAMS.DEPLOY
 
-    /*
-    script {
-
-        input {
-            id 'deploy-approval'
-            message 'Would you like to deploy?'
-            ok 'Deploy'
-            submitterParameter 'deploy-approved-by'
-            parameters {
-                agentParameter('TARGET_NAME')
+                echo "All parameters have been set as Environment Variables"
+                echo "Selected Environment: ${env.ENVIRONMENT}"
+                echo "Selected Tag: ${env.IMAGE_TAG}"
             }
+
         }
 
-    }*/
+        stage("Approve or Abort") {
+
+            script {
+                input id: 'Btn', message: 'Test', ok: 'OK', submitter: 'approve', submitterParameter: 'store'
+            }
+
+        }
+
+        stage("Run Execution") {
+
+            script {
+                input message: 'Test', parameters: [run(filter: 'STABLE', name: 'Parâmetro de execução', projectName: 'http://localhost:8080/job/core-fw/job/INPUTS-TESTS/')]
+            }
+
+        }
+
+    }
 
 }
