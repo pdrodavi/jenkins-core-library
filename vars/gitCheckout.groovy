48328e4b106a9766c96ccc04962e2258e681f6a6
Pedro Davi <pedrodaviti@gmail.com>
2023-03-03 16:17:51 -0300

    update

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -23,7 +23,7 @@ def call(String repo){
         echo "Branch selecionada: ${inputBranch}"
 
 //        sh "git clone https://github.com/pdrodavi/${repo}.git"
-        sh 'git clone -b ${inputBranch} https://pdrodavi:${CREDGH}@github.com/pdrodavi/${repo}.git'
+        sh 'git clone -b ' + "${inputBranch}" + 'https://pdrodavi:${CREDGH}@github.com/pdrodavi/' + "${repo}" + '.git'
 //        sh "git clone https://pdrodavi:${GITHUBRESTJWT}@github.com/pdrodavi/${repo}.git"
 
         sh "ls -a"
