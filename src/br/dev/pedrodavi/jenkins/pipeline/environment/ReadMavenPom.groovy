be1e279479b0e275c0e4cc7bd79eb07458f05dfe
Pedro Davi <pedrodaviti@gmail.com>
2023-03-03 13:46:24 -0300

    update

--- src/br/dev/pedrodavi/jenkins/pipeline/environment/ReadMavenPom.groovy
+++ src/br/dev/pedrodavi/jenkins/pipeline/environment/ReadMavenPom.groovy
@@ -3,7 +3,7 @@ package br.dev.pedrodavi.jenkins.pipeline.environment
 class ReadMavenPom {
 
     static def pv() {
-        return "${readMavenPom().getVersion()}"
+        return readMavenPom().getVersion()
     }
 
 }
