edc16968d3df4c3ff905bb6c104f61906578420b
Pedro Davi <pedrodaviti@gmail.com>
2023-03-04 01:46:18 -0300

    tests

--- vars/testsGeral.groovy
+++ vars/testsGeral.groovy
@@ -1,41 +1,27 @@
+import br.dev.pedrodavi.jenkins.pipeline.scm.GitRepository
 import br.dev.pedrodavi.jenkins.pipeline.utils.logging.Logger
 
 def call() {
 
     node {
 
-        stage("Logger") {
-              Logger test = new Logger(this)
-      // call all logger init functions
-              Logger.init(this, [:])
-              Logger.init(this, "info")
-              Logger.init(this, 0)
-              Logger.setLevel(LogLevel.ALL)
-              Logger.setLevel(LogLevel.DEBUG)
-              Logger.setLevel(LogLevel.WARN)
-              Logger.setLevel(LogLevel.ERROR)
-              Logger.setLevel(LogLevel.DEPRECATED)
-              Logger.setLevel(LogLevel.FATAL)
-              Logger.setLevel(LogLevel.INFO)
-              Logger.setLevel(LogLevel.NONE)
-              Logger.setLevel(LogLevel.TRACE)
-              // set loglevel to all
-              Logger.setLevel(LogLevel.ALL)
+        stage("GitRepository") {
+                  GitRepository gitRepo = new GitRepository(this, "https://github.com/wcm-io-devops/jenkins-pipeline-library.git")
+//                  integrationTestUtils.assertEquals(GitRepository.PROTOCOL_HTTPS, gitRepo.getProtocol())
+//                  integrationTestUtils.assertNotEquals(GitRepository.PROTOCOL_HTTP, gitRepo.getProtocol())
+//                  integrationTestUtils.assertNotEquals(GitRepository.PROTOCOL_SSH, gitRepo.getProtocol())
 
-              test.trace("trace")
-              test.debug("debug")
-              test.info("info")
-              test.warn("warn")
-              test.error("error")
-              test.fatal("fatal")
-
-              test.trace("trace", this)
-              test.debug("debug", this)
-              test.deprecated("deprecatedItem", "deprecatedMessage")
-              test.info("info", this)
-              test.warn("warn", this)
-              test.error("error", this)
-              test.fatal("fatal", this)
+                  gitRepo.getGroup()
+                  gitRepo.getProject()
+                  gitRepo.getProjectName()
+                  gitRepo.getProtocolPrefix()
+                  gitRepo.getServer()
+                  gitRepo.getUsername()
+                  gitRepo.isSsh()
+                  gitRepo.isHttp()
+                  gitRepo.isHttps()
+                  gitRepo.getUrl()
+                  gitRepo.isValid()
         }
 
     }
