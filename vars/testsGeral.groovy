386e7428e379a5cbdf66002c692d41142ecc99db
Pedro Davi <pedrodaviti@gmail.com>
2023-03-04 01:29:57 -0300

    tests

--- vars/testsGeral.groovy
+++ vars/testsGeral.groovy
@@ -1,23 +1,41 @@
-import br.dev.pedrodavi.jenkins.pipeline.managedfiles.ManagedFileConstants
 import br.dev.pedrodavi.jenkins.pipeline.utils.logging.Logger
 
 def call() {
 
     node {
 
-        Logger.init(this, LogLevel.INFO)
-        Logger log = new Logger(this)
+        stage("Logger") {
+              Logger test = new Logger(this)
+      // call all logger init functions
+              Logger.init(this, [:])
+              Logger.init(this, "info")
+              Logger.init(this, 0)
+              Logger.setLevel(LogLevel.ALL)
+              Logger.setLevel(LogLevel.DEBUG)
+              Logger.setLevel(LogLevel.WARN)
+              Logger.setLevel(LogLevel.ERROR)
+              Logger.setLevel(LogLevel.DEPRECATED)
+              Logger.setLevel(LogLevel.FATAL)
+              Logger.setLevel(LogLevel.INFO)
+              Logger.setLevel(LogLevel.NONE)
+              Logger.setLevel(LogLevel.TRACE)
+              // set loglevel to all
+              Logger.setLevel(LogLevel.ALL)
 
-        stage("ManagedFileConstants") {
-          ManagedFileConstants managedFileConstants = new ManagedFileConstants()
-          log.info(ManagedFileConstants.GLOBAL_MAVEN_SETTINGS_PATH, ManagedFileConstants.GLOBAL_MAVEN_SETTINGS_PATH)
-          log.info(ManagedFileConstants.GLOBAL_MAVEN__SETTINGS_ENV, ManagedFileConstants.GLOBAL_MAVEN__SETTINGS_ENV)
-          log.info(ManagedFileConstants.MAVEN_SETTINGS_PATH, ManagedFileConstants.MAVEN_SETTINGS_PATH)
-          log.info(ManagedFileConstants.MAVEN_SETTING_ENV, ManagedFileConstants.MAVEN_SETTING_ENV)
-          log.info(ManagedFileConstants.NPM_CONFIG_USERCONFIG_PATH, ManagedFileConstants.NPM_CONFIG_USERCONFIG_PATH)
-          log.info(ManagedFileConstants.NPM_CONFIG_USERCONFIG_ENV, ManagedFileConstants.NPM_CONFIG_USERCONFIG_ENV)
-          log.info(ManagedFileConstants.BUNDLE_CONFIG_ENV, ManagedFileConstants.BUNDLE_CONFIG_ENV)
-          log.info(ManagedFileConstants.BUNDLE_CONFIG_PATH, ManagedFileConstants.BUNDLE_CONFIG_PATH)
+              test.trace("trace")
+              test.debug("debug")
+              test.info("info")
+              test.warn("warn")
+              test.error("error")
+              test.fatal("fatal")
+
+              test.trace("trace", this)
+              test.debug("debug", this)
+              test.deprecated("deprecatedItem", "deprecatedMessage")
+              test.info("info", this)
+              test.warn("warn", this)
+              test.error("error", this)
+              test.fatal("fatal", this)
         }
 
     }
